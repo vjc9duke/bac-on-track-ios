aef766f3a13826a4b3eee595f265b7d5ba38fc1c
Vincent <vjc9@duke.edu>
2023-02-19 14:56:13 -0500

    initial push

---
+++ BAC on Track/BAC on Track/BAC_on_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAC_on_TrackApp.swift
+//  BAC on Track
+//
+//  Created by Vincent Chen on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BAC_on_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
