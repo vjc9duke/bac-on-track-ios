f42ab0f69a5b61bfd172f7debb3b9e571901bf02
Vincent <vjc9@duke.edu>
2023-02-24 23:17:34 -0500

    numpad only for certain attributes on settings page

--- BAC on Track/BAC on Track/Pages/SettingsPage.swift
+++ BAC on Track/BAC on Track/Pages/SettingsPage.swift
@@ -11,55 +11,68 @@ struct SettingsPage: View {
     @State private var name = ""
     @State private var weight = ""
     @State private var age = ""
-    @State private var gender = ""
+    @State private var gender = "Male"
     @State private var togglePlaceholder1 = false
     @State private var togglePlaceholder2 = false
     @State private var togglePlaceholder3 = false
     
+    private let genderOptions = ["Male", "Female", "Other"]
+    
     var body: some View {
         ScrollView {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Profile")
-                .font(.largeTitle)
-                .bold()
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Name")
-                    .font(.headline)
-                TextField("Enter your name", text: $name)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Profile")
+                    .font(.largeTitle)
+                    .bold()
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Name")
+                        .font(.headline)
+                    TextField("Enter your name", text: $name)
+                        .submitLabel(.done)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Text("Weight")
+                        .font(.headline)
+                    TextField("Enter your weight", text: $weight)
+                        .submitLabel(.done)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Text("Age")
+                        .font(.headline)
+                    TextField("Enter your age", text: $age)
+                        .submitLabel(.done)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Text("Gender")
+                        .font(.headline)
+                    
+                    Picker(selection: $gender, label: Text("Gender")) {
+                        ForEach(genderOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                        
+                }
                 
-                Text("Weight")
-                    .font(.headline)
-                TextField("Enter your weight", text: $weight)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Divider()
                 
-                Text("Age")
-                    .font(.headline)
-                TextField("Enter your age", text: $age)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Text("Settings")
+                    .font(.largeTitle)
+                    .bold()
                 
-                Text("Gender")
-                    .font(.headline)
-                TextField("Enter your gender", text: $gender)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Toggle("Placeholder 1", isOn: $togglePlaceholder1)
+                                
+                Toggle("Placeholder 2", isOn: $togglePlaceholder2)
+                
+                Toggle("Placeholder 3", isOn: $togglePlaceholder3)
+                 
+                Spacer()
             }
-            
-            Divider()
-            
-            Text("Settings")
-                .font(.largeTitle)
-                .bold()
-            
-            Toggle("Placeholder 1", isOn: $togglePlaceholder1)
-                            
-            Toggle("Placeholder 2", isOn: $togglePlaceholder2)
-            
-            Toggle("Placeholder 3", isOn: $togglePlaceholder3)
-             
-            Spacer()
-        }
-        .padding()
-    }
+            .padding()
+        }.padding(.top, UIScreen.main.focusedView?.safeAreaInsets.top)
     }
 }
 
